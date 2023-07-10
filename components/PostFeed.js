d72acd7d5e2bc437f055d5bf0061f420209529bd
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-07-11 00:11:03 +0100

    error

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,11 +1,14 @@
+
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Tweet from "./Tweet";
 import TweetInput from "./TweetInput";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "@component/firebase";
 
-export default function PostsFeed() {
+export default function PostFeed() {
   const [tweets, setTweets] = useState([]);
+
   useEffect(() => {
     const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
     const unsubscribe = onSnapshot(q, (snapshot) => {
@@ -17,23 +20,23 @@ export default function PostsFeed() {
 
   return (
     <div
-      className="sm:ml-16 xl:ml-80 max-w-2xl flex-grow
-        border-gray-700 border-x"
+      className="sm:ml-16 xl:ml-[350px] max-w-2xl flex-grow
+        border-gray-700 border-x
+        "
     >
-      Feed
       <div
         className="px-3 py-2 text-lg sm:text-xl font-bold
-            border-b border-gray-700 sticky top-0 z-50"
+            border-b border-gray-700 sticky top-0 z-50
+            "
       >
         Home
       </div>
       <TweetInput />
-      {
-        tweets.map(tweet => {
-            return <Tweet key={tweets.id} data={tweet.data()}/>
-        })
-      }
-      <Tweet />
+
+      {tweets.map((tweet) => {
+        return <Tweet key={tweet.id} id={tweet.id} data={tweet.data()} />;
+      })}
+
     </div>
   );
 }
--- firebase.js
+++ firebase.js
@@ -2,20 +2,22 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAa01wR8185W25jcWnctMwJlGf6sGNi44U",
-  authDomain: "twitter-clone-bd221.firebaseapp.com",
-  projectId: "twitter-clone-bd221",
-  storageBucket: "twitter-clone-bd221.appspot.com",
-  messagingSenderId: "674375007193",
-  appId: "1:674375007193:web:2f688ad737dfa7f93f77c8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const auth = getAuth(app)
+export const storage = getStorage()
